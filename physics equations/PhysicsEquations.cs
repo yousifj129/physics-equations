3de6cfd4c90ea804945bd6cdf87108f730e0b1b1
Wyperian <138237408+yousifj129@users.noreply.github.com>
2023-08-09 21:33:44 +0300

    added some constant values

---
+++ physics equations/PhysicsEquations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace physics_equations
+{
+    public class PhysicsEquations
+    {
+        // Gravitational constant (units: m^3 kg^-1 s^-2)
+        public const double GravitationalConstant = 6.67430e-11;
+
+        // Planck constant (units: J s)
+        public const double PlanckConstant = 6.62607015e-34;
+
+        // Speed of light in a vacuum (units: m/s)
+        public const double SpeedOfLight = 299792458;
+
+        // Elementary charge (units: C)
+        public const double ElementaryCharge = 1.602176634e-19;
+
+        // Avogadro's number (units: mol^-1)
+        public const double AvogadroNumber = 6.02214076e23;
+
+        // Boltzmann constant (units: J K^-1)
+        public const double BoltzmannConstant = 1.380649e-23;
+
+        // Electron mass (units: kg)
+        public const double ElectronMass = 9.10938356e-31;
+
+        // Proton mass (units: kg)
+        public const double ProtonMass = 1.67262192369e-27;
+
+        // Speed of sound in air at 20°C (units: m/s)
+        public const double SpeedOfSoundInAir = 343;
+
+        // Universal gas constant (units: J K^-1 mol^-1)
+        public const double UniversalGasConstant = 8.314462618;
+
+        // Stefan-Boltzmann constant (units: W m^-2 K^-4)
+        public const double StefanBoltzmannConstant = 5.670374419e-8;
+    }
+}
--- physics equations/Program.cs
+++ physics equations/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection;
+
+
+Console.WriteLine("Enter function name (a, b, or c):");
+string functionName = Console.ReadLine();
+
+// Get the method info using the function name
+MethodInfo method = typeof(Program).GetMethod(functionName, BindingFlags.Static | BindingFlags.NonPublic);
+
+// Check if the method exists
+if (method != null)
+{
+    // Invoke the method
+    method.Invoke(null, null);
+}
+else
+{
+    Console.WriteLine("Invalid function name");
+}
